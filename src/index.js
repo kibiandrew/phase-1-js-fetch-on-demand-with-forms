5db7c7b52a1c409244b2a68cb1b7ed275fdac11b
Andrew Kibiwott <kibiandrew@gmail.com>
2022-12-14 09:04:10 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
+    const inputForm=  document.querySelector('form').addEventListener('submit',(e)=>{
+          e.preventDefault();
+          const inputEl=document.getElementById('searchByID');
+          console.log(inputEl.value)
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+          fetch(`http://localhost:3000/movies/${inputEl.value}`)
+          .then(res=>res.json())
+          .then(data=>{
+              const title = document.querySelector('section#movieDetails h4');
+              const summary = document.querySelector('section#movieDetails p');
+  
+               title.innerText = data.title;
+              summary.innerText = data.summary;
+          })
+      })
+    
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
